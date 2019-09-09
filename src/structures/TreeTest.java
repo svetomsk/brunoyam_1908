364e3090fe8a3e9e47d4a91eeca25094fffadbef
123 <123>
2019-09-09 20:41:34 +0300

    Tree walk implemented

--- src/structures/TreeTest.java
+++ src/structures/TreeTest.java
@@ -4,11 +4,41 @@ public class TreeTest {
     public static void main(String[] args) {
         Item ten = new Item(null, null, 10);
 
+        // все переменные ниже использовать нельзя
         Item eight = new Item(null, null, 8);
         Item thirteen = new Item(null, null, 13);
         ten.setLeft(eight);
         ten.setRight(thirteen);
 
+        Item one = new Item(null, null, 1);
+        Item nine = new Item(null, null, 9);
+        Item twelve = new Item(null, null, 12);
+        Item fifteen = new Item(null, null, 15);
+        eight.setLeft(one);
+        eight.setRight(nine);
+        thirteen.setLeft(twelve);
+        thirteen.setRight(fifteen);
 
+
+//        System.out.println(ten.getValue());
+//        if(ten.getLeft() != null) {
+//            System.out.println(ten.getLeft().getValue());
+//
+//            if (ten.getLeft().getLeft() != null) {
+//                System.out.println(ten.getLeft().getLeft().getValue());
+//            }
+//        }
+        print(ten);
+    }
+
+    public static void print(Item current) {
+        System.out.println(current.getValue());
+
+        if(current.getLeft() != null) {
+            print(current.getLeft());
+        }
+        if(current.getRight() != null) {
+            print(current.getRight());
+        }
     }
 }
